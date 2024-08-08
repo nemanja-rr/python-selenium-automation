bb6f21b72329c075137c6862f13773a9e664d630
Nemanja <nemanja.simic@redrhino.com>
2024-08-07 22:02:00 -0400

    HW #8 UPDATED

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,9 @@
 import sys
+import ssl
+import os
+from allure_behave.utils import scenario_name
 from selenium import webdriver
-from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,13 +12,13 @@ from app.application import Application
 from time import sleep
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    driver_path = './chromedriver' # for macOS users
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    # driver_path = './chromedriver' # for macOS users
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
 
     # # HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
@@ -27,6 +30,23 @@ def browser_init(context):
     #     service=service
     # )
 
+    ###BROWSER STACK#################################
+
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'nemanjasimic_n1NRXu'
+    bs_key = 'z7sz4eLYcBxscCLyi9GV'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os": "OS X",
+        "osVersion": "Sonoma",
+        'browserName': 'chrome',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.wait = WebDriverWait(context.driver, 10)
@@ -35,14 +55,8 @@ def browser_init(context):
 
 
 
-# def before_all(context):
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('headless')
-#     service = Service(ChromeDriverManager().install())
-#     context.driver = webdriver.Chrome(options=options, service=service)
-#     context.driver.maximize_window()
-#     context.driver.implicitly_wait(10)
-#     context.app = Application(context.driver)
+
+############################################################
 
 # def after_all(context):
 #     context.driver.quit()
@@ -50,7 +64,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
